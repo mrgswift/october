4134370496b9a96b58e6bb594e74fb4bc0f1813e
Samuel Georges <sam@daftspunk.com>
2025-03-16 17:42:11 +1100

    Update version

--- modules/system/classes/UpdateManager.php
+++ modules/system/classes/UpdateManager.php
@@ -14,7 +14,7 @@ class UpdateManager
     /**
      * @var string WANT_VERSION is the default composer version string to use.
      */
-    const WANT_VERSION = '^3.7';
+    const WANT_VERSION = '^4.0@beta';
 
     /**
      * Requests details about a project based on its identifier.
